6befdbe18fabe55aa96689f0b392db7e2446ef5c
Absolom Mugwagwa <absolom.mugwagwa@yoyowallet.com>
2021-05-05 17:02:02 +0200

    add models

---
+++ app/Models/Video.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Video extends Model
+{
+    use HasFactory;
+
+    // protected $guarded = ['id'];
+    protected $fillable = ['title', 'excerpt', 'body', 'screencast', 'tutorial_id'];
+
+    public function tutorial()
+    {
+        return $this->belongsTo(Tutorial::class);
+    }
+}
